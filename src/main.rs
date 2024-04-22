a38ab9af968a50cdecf2e164f8e3a9a711bcf84b
Filip Lange <filip.lange@gmail.com>
2024-04-22 22:45:30 +0200

    Now prints out the came variable

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,15 @@ struct Day {
     lunch: Option<TimeDelta>,
 }
 
+impl Day {
+    fn start_string(&self) -> String {
+        match self.start {
+            Some(time) => time.format("%H:%M").to_string(),
+            None => String::new(),
+        }
+    }
+}
+
 enum ParsedDay {
     Day(Day),
     ParseError(String),
@@ -48,27 +57,24 @@ fn parse_came(args: &Vec<String>) -> Result<Option<String>, String> {
     }
 }
 
+fn parse_time(text: &str) -> Result<NaiveDateTime, String> {
+    let today = Local::now();
+    let today_string = today.format("%Y-%m-%d").to_string();
+    let time_string = format!("{} {}", today_string, text);
+    match NaiveDateTime::parse_from_str(&time_string, "%Y-%m-%d %H:%M") {
+        Ok(dt) => Ok(dt),
+        Err(e) => Err(format!("Could not parse date string {}. Error: {}", text, e).to_string()),
+    }
+}
+
 fn parse_args(args: &Vec<String>) -> ParsedDay {
     let start = match parse_came(args) {
-        // Ok(option) => match option {
-        //     Some(text) => match NaiveDateTime::parse_from_str(&text, "%H:%M") {
-        //         Err(_) => None,
-        //         Ok(value) => value,
-        // },
         Ok(option) => match option {
             None => None,
-            Some(text) => {
-                println!("{text}");
-                match NaiveDateTime::parse_from_str(&text, "%H:%M") {
-                    Ok(dt) => Some(dt),
-                    Err(e) => {
-                        return ParsedDay::ParseError(
-                            format!("Could not parse date string {}. Error: {}", text, e)
-                                .to_string(),
-                        )
-                    }
-                }
-            }
+            Some(text) => match parse_time(&text) {
+                Ok(dt) => Some(dt),
+                Err(e) => return ParsedDay::ParseError(e),
+            },
         },
         Err(error) => return ParsedDay::ParseError(error),
     };
@@ -85,7 +91,13 @@ fn parse_args(args: &Vec<String>) -> ParsedDay {
 
 fn print_current_week(day: Day) {
     let mut builder = Builder::default();
-    builder.push_record(days_in_current_week());
+    let mut top_row = days_in_current_week();
+    top_row.insert(0, "".to_string());
+    builder.push_record(top_row);
+    let mut start_row = vec!["came"];
+    let start_string = day.start_string();
+    start_row.push(&start_string);
+    builder.push_record(start_row);
     let table = builder.build().to_string();
     println!("{table}")
 }
