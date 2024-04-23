133654276b1322ce9a638ea2d5dbe0e899289e29
Filip Lange <filip.lange@gmail.com>
2024-04-23 21:57:29 +0200

    Now inserts the input time in a hashmap and shows the current week

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,32 @@
 use chrono::prelude::*;
 use chrono::TimeDelta;
-// use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
+use std::collections::HashMap;
 // use std::collections::HashMap;
 use std::env;
-use std::io;
-use tabled::{
-    builder::Builder,
-    settings::{object::Rows, Alignment, Modify, Style},
-};
+use std::fs::File;
+use std::io::Read;
+use tabled::builder::Builder;
 
+#[derive(Deserialize)]
 struct Day {
     start: Option<NaiveDateTime>,
     stop: Option<NaiveDateTime>,
+    #[serde(deserialize_with = "deserialize_timedelta")]
     lunch: Option<TimeDelta>,
+}
+
+struct SerializableDay {
+    start: Option<String>,
+    stop: Option<String>,
+    lunch: Option<String>,
+}
+fn deserialize_timedelta<'de, D>(deserializer: D) -> Result<Option<TimeDelta>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let seconds = i64::deserialize(deserializer)?;
+    Ok(Some(TimeDelta::seconds(seconds)))
 }
 
 impl Day {
@@ -29,21 +43,14 @@ enum ParsedDay {
     ParseError(String),
 }
 
-fn days_in_current_week() -> Vec<String> {
+fn days_in_current_week() -> Vec<NaiveDate> {
     let offset = Local::now().weekday().num_days_from_monday();
     let timedelta_to_last_monday = TimeDelta::try_days(-i64::from(offset)).unwrap();
     let date_of_last_monday = Local::now() + timedelta_to_last_monday;
     let timedeltas = (0..=4)
         .map(|i| TimeDelta::try_days(i).unwrap())
         .map(|d| date_of_last_monday + d)
-        .map(|datetime| {
-            datetime
-                .to_rfc3339()
-                .split_terminator("T")
-                .next()
-                .unwrap()
-                .to_string()
-        });
+        .map(|datetime| datetime.date_naive());
     timedeltas.collect::<Vec<_>>()
 }
 
@@ -89,24 +96,52 @@ fn parse_args(args: &Vec<String>) -> ParsedDay {
     }
 }
 
-fn print_current_week(day: Day) {
+fn print_current_week(days: HashMap<NaiveDate, Day>) {
     let mut builder = Builder::default();
-    let mut top_row = days_in_current_week();
+    let week_days = days_in_current_week();
+    let mut top_row: Vec<String> = week_days
+        .iter()
+        .map(|date| date.format("%Y-%m-%d").to_string())
+        .collect();
     top_row.insert(0, "".to_string());
     builder.push_record(top_row);
-    let mut start_row = vec!["came"];
-    let start_string = day.start_string();
-    start_row.push(&start_string);
+
+    let starts: Vec<String> = week_days
+        .iter()
+        .map(|date| match days.get(date) {
+            None => "".to_string(),
+            Some(day) => match day.start {
+                None => "".to_string(),
+                Some(dt) => dt.format("%H:%M").to_string(),
+            },
+        })
+        .collect();
+    let mut start_row = vec!["came".to_string()];
+    start_row.extend(starts);
     builder.push_record(start_row);
     let table = builder.build().to_string();
     println!("{table}")
 }
 
+fn load_days(path: &str) -> HashMap<NaiveDate, Day> {
+    let mut file = File::open(path).expect("Failed to open days.json");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect(&format!("Failed to read {}", path));
+    serde_json::from_str(&contents).expect("Failed to parse JSON")
+}
+
 fn main() {
+    let mut days = load_days("days.json");
     let args: Vec<_> = env::args().collect();
     let parsed_day = parse_args(&args);
+    let local_date = Local::today();
+    let naive_date = local_date.naive_local();
     match parsed_day {
-        ParsedDay::Day(day) => print_current_week(day),
         ParsedDay::ParseError(description) => println!("{description}"),
+        ParsedDay::Day(day) => {
+            days.insert(naive_date, day);
+            print_current_week(days);
+        }
     }
 }
