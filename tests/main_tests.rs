4b84e059440a77b8873bf8e7a94c8c025da681c3
Filip Lange <filip.lange@gmail.com>
2024-05-16 23:19:42 +0200

    Add rstest crate and create failing test case for keeping old data in existing days

--- tests/main_tests.rs
+++ tests/main_tests.rs
@@ -1,28 +1,36 @@
+use std::path::{Path, PathBuf};
+
 use chrono::Local;
-use tempfile::tempdir;
+use rstest::*;
+use tempfile::{tempdir, TempDir};
 use timereport::main;
 
-fn run(s: &str) -> String {
-    let dir = tempdir().expect("Could not create tempdir");
-    let file_path = dir.path().join("timereport.json");
+#[fixture]
+fn temp_dir() -> TempDir {
+    println!("inside&temp_dir");
+    tempdir().expect("Could not create tempdir")
+    //dir.path().join("timereport.json")
+}
+
+fn run(s: &str, temp_dir: &TempDir) -> String {
     main(
         s.split_whitespace().map(|s| s.to_string()).collect(),
-        file_path.as_path(),
+        &temp_dir.path().join("timereport.json").as_path(),
     )
 }
 
-#[test]
-fn no_arguments_prints_current_date() {
+#[rstest]
+fn no_arguments_prints_current_date(temp_dir: TempDir) {
     let today = Local::now().format("%Y-%m-%d").to_string();
 
-    let output = run("--weekend");
+    let output = run("--weekend", &temp_dir);
 
     assert!(output.contains(&today));
 }
 
-#[test]
-fn start_2024_06_26_at_8_30_prints_table() {
-    let output = run("2024-04-26 start 8:30");
+#[rstest]
+fn start_2024_06_26_at_8_30_prints_table(temp_dir: TempDir) {
+    let output = run("2024-04-26 start 8:30", &temp_dir);
     let expected = r#"+-------+------------+------------+------------+------------+------------+
 |       | 2024-04-22 | 2024-04-23 | 2024-04-24 | 2024-04-25 | 2024-04-26 |
 +-------+------------+------------+------------+------------+------------+
@@ -35,9 +43,9 @@ fn start_2024_06_26_at_8_30_prints_table() {
     assert_eq!(output, expected);
 }
 
-#[test]
-fn stop() {
-    let output = run("2024-04-26 stop 8:30");
+#[rstest]
+fn stop(temp_dir: TempDir) {
+    let output = run("2024-04-26 stop 8:30", &temp_dir);
     let expected = r#"+-------+------------+------------+------------+------------+------------+
 |       | 2024-04-22 | 2024-04-23 | 2024-04-24 | 2024-04-25 | 2024-04-26 |
 +-------+------------+------------+------------+------------+------------+
@@ -50,31 +58,39 @@ fn stop() {
     assert_eq!(output, expected);
 }
 
-#[test]
-fn start_8_30_weekend_prints_current_date_and_8_30() {
+#[rstest]
+fn start_8_30_weekend_prints_current_date_and_8_30(temp_dir: TempDir) {
     let today = Local::now().format("%Y-%m-%d").to_string();
 
-    let output = run("start 8:30 --weekend");
+    let output = run("start 8:30 --weekend", &temp_dir);
 
     assert!(output.contains(&today));
     assert!(output.contains("08:30"));
 }
 
-#[test]
-fn lunch_HHcolonMM() {
+#[rstest]
+fn lunch_HHcolonMM(temp_dir: TempDir) {
     let today = Local::now().format("%Y-%m-%d").to_string();
 
-    let output = run("lunch 1:75 --weekend");
+    let output = run("lunch 1:75 --weekend", &temp_dir);
 
     assert!(output.contains(&today));
     assert!(output.contains("2:15"));
 }
 
-#[test]
-fn lunch_MMm() {
+#[rstest]
+fn subsequent_edits_of_the_same_day(temp_dir: TempDir) {
+    run("start 7:00 --weekend", &temp_dir);
+    let output = run("stop 15:00 --weekend", &temp_dir);
+    assert!(output.contains("7:00"));
+    assert!(output.contains("15:00"));
+}
+
+#[rstest]
+fn lunch_MMm(temp_dir: TempDir) {
     let today = Local::now().format("%Y-%m-%d").to_string();
 
-    let output = run("lunch 15m --weekend");
+    let output = run("lunch 15m --weekend", &temp_dir);
 
     assert!(output.contains(&today));
     assert!(output.contains("0:15"));
